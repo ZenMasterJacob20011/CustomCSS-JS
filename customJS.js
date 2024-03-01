61a96e82d8bdc756517bfc8e9d2abc96791874d8
jsnichols <32579287+ZenMasterJacob20011@users.noreply.github.com>
2024-03-01 09:54:42 -0600

    Update customJS.js

--- customJS.js
+++ customJS.js
@@ -1,1 +1,28 @@
-alert("I just ran");
+class myInput extends Input{
+    static schema(...extend){
+        return Input.schema({
+            type: 'mycomp',
+            label: 'My Component',
+            key: 'mycomp'
+        });
+    }
+
+    static get builderInfo(){
+        return {
+            title: 'My Component',
+            icon: 'terminal',
+            group: 'basic',
+            documentation: '/userguide/#textfield',
+            weight: 0,
+            schema: myInput.schema()
+        }
+    }
+
+    constructor(component, options, data) {
+        super(component, options, data);
+    }
+
+    init(){
+        super.init();
+    }
+}
